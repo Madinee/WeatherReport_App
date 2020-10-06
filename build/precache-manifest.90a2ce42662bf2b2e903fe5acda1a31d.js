46eec2fc0aa44dc61e490bc0fb4f5631ff928084
Madinee <foumtraore22@gmail.com>
2020-10-06 20:49:55 +0200

    deploy app

---
+++ build/precache-manifest.90a2ce42662bf2b2e903fe5acda1a31d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b00952d6cf1851b040955ba5062711f",
+    "url": "/WeatherReport_App/index.html"
+  },
+  {
+    "revision": "8e3e6fff21f2950e7a83",
+    "url": "/WeatherReport_App/static/css/main.1c3a0d5e.chunk.css"
+  },
+  {
+    "revision": "8503dc8a89af0a310930",
+    "url": "/WeatherReport_App/static/js/2.1f3f651e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherReport_App/static/js/2.1f3f651e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3e6fff21f2950e7a83",
+    "url": "/WeatherReport_App/static/js/main.5252b35f.chunk.js"
+  },
+  {
+    "revision": "f3eba29cd488f2cbd706",
+    "url": "/WeatherReport_App/static/js/runtime-main.76f3d611.js"
+  }
+]);
